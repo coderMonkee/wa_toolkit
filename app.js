03ca3fe8707eab6e53f729f0ce22f70a80898a53
coderMonkee <codermonkee@gmail.com>
2024-03-26 22:34:02 +0530

    new work

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const axios = require('axios');
 var bodyParser = require('body-parser')
-const { randomDigit, extractDetails, findOperatorCode } = require('./function');
+const { randomDigit, extractDetails, findOperatorCode, parseMessage } = require('./function');
 // Create an instance of Express
 const app = express();
 const port = 3000; // Choose the port you want to run your server on
@@ -172,6 +172,113 @@ app.post('/wa-recharge', async (req, res) => {
     const {m_content ,m_phone}  = req.body
     let operator_code;
     let circle_code;
+    const { number, operator, circle, amount } = parseMessage(m_content);
+    const usertx = randomDigit()
+    const operatorCode = findOperatorCode(operator);
+
+    operator_code = operator
+    circle_code = circle 
+
+    if(!number || !amount){
+      const newResponse =  await sendMessageToWhatsApp({
+        message:`Please provide valid number and amount`,
+        phone:m_phone
+      })
+      console.log(newResponse,"Please provide valid number and amount Message");
+      res.status(402).json({
+        status: true,
+        message: `Please provide valid number and amount`,
+        data: newResponse,
+      });
+    }
+
+    if(!operator || !circle){
+
+      const payload = {
+        APIID: 'AP420123',
+        PASSWORD: '8878E7F4D8',
+        MOBILENUMBER:number
+      }
+
+      const response = await axios.get('https://cyrusrecharge.in/API/CyrusOperatorFatchAPI.aspx', {
+        params: payload
+      });
+
+      console.log(response.data,"Operatord details");
+
+      if(response.data){
+        const {OperatorCode , CircleCode} =  response.data 
+        operator_code= OperatorCode
+        circle_code= CircleCode
+      }
+    }
+
+    const payload = {
+      memberid: 'AP420123',
+      pin: '36FDD0063D',
+      number,
+      operator :operator_code,
+      circle:circle_code,
+      amount,
+      usertx,
+      format: 'json'
+    }
+
+    console.log(`Recharge Request: ${usertx}`, payload);
+
+    const response = await axios.get('https://cyrusrecharge.in/api/recharge.aspx', {
+      params: payload
+    });
+
+    console.log(`Recharge successful. Response: ${usertx}`, response.data);
+
+    const { ApiTransID,Status ,ErrorMessage,OperatorRef,TransactionDate} = response.data 
+
+    if(Status=='Success'){ 
+     const newResponse =  await sendMessageToWhatsApp({
+        message:`Recharge of ${amount} rupees successful for number ${number} with trasaction ref ${ApiTransID}.`,
+        phone:m_phone
+      })
+      console.log(newResponse,"Send  Success Message");
+      res.status(200).json({
+        status: true,
+        message: `Recharge of ${amount} rupees successful for number ${number} with trasaction ref ${ApiTransID}.`,
+        data: response.data,
+      });
+    }else{ 
+      const newResponse =  await sendMessageToWhatsApp({
+        message:`Recharge of ${amount} rupees $${ErrorMessage} for number ${number}.`,
+        phone:m_phone
+      })
+      console.log(newResponse,"Send  failed Message");
+      res.status(200).json({
+        status: true,
+        message: `Recharge of ${amount} rupees failed for number ${number}.`,
+        data: response.data,
+      });
+    }
+
+  
+  } catch (error) {
+
+    console.error('Error occurred during recharge:', error);
+
+    res.status(400).json({
+      status: 400,
+      statusText: "BAD_REQUEST",
+      message: "Error occurred during recharge",
+    });
+  }
+}); 
+
+app.post('/wa-recharge-old', async (req, res) => {
+
+  try {
+    console.log("Query  parameter post",req.body);
+
+    const {m_content ,m_phone}  = req.body
+    let operator_code;
+    let circle_code;
     const { number, operator, circle, amount } = extractDetails(m_content);
     const usertx = randomDigit()
     const operatorCode = findOperatorCode(operator);
@@ -262,13 +369,41 @@ app.post('/wa-recharge', async (req, res) => {
 app.get('/callback', async (req, res) => {
 
   try {
+//FAILURE
+    console.log(`Recharge callback .`,req.query); 
 
-    console.log(`Recharge callback .`,req.query);
+    const { TransID,Status ,Amount:amount,OperatorRef,ErrorCode} = req.query
+
+  
+    // if(Status=='FAILURE'){ 
+    //   const newResponse =  await sendMessageToWhatsApp({
+    //      message:`Recharge of ${amount} rupees ${ErrorCode} for number ${number} with trasaction ref ${TransID}.`,
+    //      phone:m_phone
+    //    })
+    //    console.log(newResponse,"Send  Success Message");
+    //    res.status(200).json({
+    //      status: true,
+    //      message: `Recharge of ${amount} rupees ${ErrorCode} for number ${number} with trasaction ref ${TransID}.`,
+    //      data: response.data,
+    //    });
+    //  }else{ 
+    //    const newResponse =  await sendMessageToWhatsApp({
+    //      message:`Recharge of ${amount} rupees $${ErrorMessage} for number ${number}.`,
+    //      phone:m_phone
+    //    })
+    //    console.log(newResponse,"Send  failed Message");
+    //    res.status(200).json({
+    //      status: true,
+    //      message: `Recharge of ${amount} rupees ${ErrorCode} for trasaction ${TransID}`,
+    //      data: ErrorCode,
+    //    });
+    //  }
+
 
     res.status(200).json({
       status: true,
-      message: 'Recharge successful',
-      data: response.data,
+      message: ErrorCode,
+      data: ErrorCode,
     })
 
     
